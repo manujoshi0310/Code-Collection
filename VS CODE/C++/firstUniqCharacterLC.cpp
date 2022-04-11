45241dd3ec7fb5ae04b2b09b6891d03091f8f6c4
manujoshi0310 <manu.shandillya@gmail.com>
2022-04-11 13:52:03 +0530

    add sortedSquaresLC

--- VS CODE/C++/firstUniqCharacterLC.cpp
+++ VS CODE/C++/firstUniqCharacterLC.cpp
@@ -3,30 +3,9 @@
 using namespace std;
 
 int firstUniqCharacter(string s){
-    vector<int> vec;
+    vector<vector<int>> vec;
     int x, count;
-    for(int i=0;i<s.length();i++){
-        vec.push_back(0);
-    }
-    for(int i=0;i<s.length();i++){
-        x=((int)s[i])%s.length();
-        if(vec[x]==0){
-            vec[x]++;
-        }
-        else{
-            int j = x;
-            while(vec[j]!=0){
-                j++;
-                if(j==s.length()){
-                    j=0;
-                }
-                if(j==x){
-                    break;
-                }
-            }
-
-        }
-    }
+    
 }
 
 int main(){
