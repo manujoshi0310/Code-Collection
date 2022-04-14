c2c533b1760abc1eac92299548333e998972de33
manujoshi0310 <manu.shandillya@gmail.com>
2022-04-14 22:48:31 +0530

    test

--- TryAndError/test.cpp
+++ TryAndError/test.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main(){
     vector<int> vec;
     vec.push_back(6);
+    string str1 = "manu",str;
+    int n=15;
+    sort(str1.begin(), str1.end());
 
-    cout << (int)'a';
+    cout <<floor(sqrt(n));
 }
