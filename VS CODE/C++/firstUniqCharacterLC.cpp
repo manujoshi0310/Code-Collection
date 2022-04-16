260125dda562ca8fc11696de9cb77136d23f0386
manujoshi0310 <manu.shandillya@gmail.com>
2022-04-16 16:04:58 +0530

    complete firstUniqChar LC

--- VS CODE/C++/firstUniqCharacterLC.cpp
+++ VS CODE/C++/firstUniqCharacterLC.cpp
@@ -3,11 +3,24 @@
 using namespace std;
 
 int firstUniqCharacter(string s){
-    vector<vector<int>> vec;
-    int x, count;
+    int hash[26];
+    for(int i=0;i<26;i++){
+        hash[i] = 0;
+    }
+    for(int i=0;i<s.length();i++){
+        hash[((int)s[i])%26]++;
+    }
+    for(int i=0;i<s.length();i++){
+        if(hash[((int)s[i])%26]==1){
+            return i;
+        }
+    }
+    return -1;
     
 }
 
 int main(){
+    string s = "helloworld";
+    cout << firstUniqCharacter(s);
     return 0;
 }
