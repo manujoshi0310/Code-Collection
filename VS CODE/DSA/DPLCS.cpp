56112c0e9dd888f5545f229f58ca15396d32c1a4
manujoshi0310 <manu.shandillya@gmail.com>
2022-07-20 22:32:58 +0530

    DP LCS p1

---
+++ VS CODE/DSA/DPLCS.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int t[101][101];
+
+int LCS(string X, string Y, int m, int n){
+    if(n==0||m==0)return 0;
+    if(X[m-1]==Y[n-1]){
+        if(t[m][n]!=-1)return t[m][n];
+        t[m][n] = 1 + LCS(X,Y,m-1,n-1);
+        return t[m][n];
+    }
+    else{
+        if(t[m-1][n]!=-1 && t[m][n-1]!=-1)return max(t[m][n-1],t[m-1][n]);
+        t[m-1][n] = LCS(X,Y,m-1,n);
+        t[m][n-1] = LCS(X,Y,m,n-1);
+        return max(t[m][n-1],t[m-1][n]);
+    }
+}
+
+int main(){
+    memset(t, -1, sizeof(t));
+    string X, Y;
+    cout << "Enter string X:";
+    cin >> X;
+    cout << "Enter string Y:";
+    cin >> Y;
+    int m=X.size(), n=Y.size();
+    cout << LCS(X,Y,m,n);
+    return 0;
+}
