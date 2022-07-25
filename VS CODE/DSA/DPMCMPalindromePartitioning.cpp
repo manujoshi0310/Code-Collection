ab689d7bd0fa67e73f75018eed9ff6d94d3ba7f6
manujoshi0310 <manu.shandillya@gmail.com>
2022-07-25 19:37:51 +0530

    modify MCM problem 1

--- VS CODE/DSA/DPMCMPalindromePartitioning.cpp
+++ VS CODE/DSA/DPMCMPalindromePartitioning.cpp
@@ -2,30 +2,40 @@
 
 using namespace std;
 
+static int t[1001][1001];
+
 bool isPalindrome(string s, int i, int j){
-    for(int k=i;k<=j;k++){
-        if(s[k]!=s[j-k]){
+    if(i==j)return true;
+    if(i>j) return true;
+    while(i<j){
+        if(s[i]!=s[j]){
             return false;
         }
+        i++;
+        j--;
     }
     return true;
 }
 
 int solve(string s, int i, int j){
     if(i>=j)return 0;
-    if(isPalindrome(s,i,j)) return 0;
+    if(isPalindrome(s,i,j)==true) return 0;
+    if(t[i][j]!=-1)return t[i][j];
     int mn = INT_MAX, temp;
     for(int k=i;k<=j-1;k++){
-        temp = 1+solve(s,i,k)+solve(s,k+1,j);
-        mn = min(mn,temp);
+        temp = solve(s,i,k)+solve(s,k+1,j)+1;
+        if(mn>temp){
+            mn=temp;
+        }
     }
+    t[i][j]=mn;
     return mn;
 }
 
 int main(){
+    memset(t,-1,sizeof(t));
     string s;
     cout << "Enter the string:";
-    // cin >> s;
-    s="nitik";
+    cin >> s;
     cout << solve(s,0,s.size()-1);
 }
