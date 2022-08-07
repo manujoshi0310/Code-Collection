d41089224f1db19c8218392353cbd4cfda1f4552
manujoshi0310 <manu.shandillya@gmail.com>
2022-08-07 22:10:48 +0530

    add str problem4

---
+++ VS CODE/DSA/StrRotationOfOneAnother.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool isRotation(string a, string b){
+    if(a.size()!=b.size())return false;
+    a +=a;
+    return(a.find(b)!=string::npos);
+
+}
+
+int main(){
+    cout<<isRotation("cdba","abcd");
+    return 0;
+}
