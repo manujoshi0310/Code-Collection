caff9a6fae71c29b0ac339560ea6974b3b61493b
manujoshi0310 <manu.shandillya@gmail.com>
2022-08-22 01:07:39 +0530

    add python oops

--- VS CODE/Python/test1.py
+++ VS CODE/Python/test1.py
@@ -1,5 +1,10 @@
 class Item:
-    def __init__(self, name):       #CONSTRUCTOR
+    discount = 0.85
+    def __init__(self, name, price, quantity):       #CONSTRUCTOR
         self.name = name
-
-#LEFT AT 19.23
+        self.price = price
+        self.quantity = quantity
+    def totalPrice(self, price, quantity):
+        return price*quantity
+    def applyDiscount(self):
+        self.discount * self.totalPrice(self.price, self.quantity)
