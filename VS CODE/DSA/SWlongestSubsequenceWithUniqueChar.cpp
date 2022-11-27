662c5b541a3a4f96687bbcc40e0d0d4b4810f651
manujoshi0310 <manu.shandillya@gmail.com>
2022-11-28 00:06:15 +0530

    add Variable Sliding Window2

--- VS CODE/DSA/SWlongestSubsequenceWithUniqueChar.cpp
+++ VS CODE/DSA/SWlongestSubsequenceWithUniqueChar.cpp
@@ -7,24 +7,24 @@ int longestSubsequenceWithUniqueChar(string s, int k){
     int i=0,j=0,maxm=0;
     while(j<s.size()){
         mp[s[j]]++;
-        if(mp.size()<k){
-            j++;
-        }
-        if(mp.size()==k){
-            maxm=max(maxm,j-i+1);
-            j++;
-        }
+        if(mp.size()==k)maxm=max(maxm,j-i+1);
+        
         if(mp.size()>k){
             while(mp.size()>k){
                 mp[s[i]]--;
                 if(mp[s[i]]==0){
                     mp.erase(s[i]);
                 }
+                i++;
             }
+            if(mp.size()==k)maxm = max(maxm, j-i+1);
         }
+        j++;
     }
+    return maxm;
 }
 
 int main(){
+    cout << longestSubsequenceWithUniqueChar("aaaa",1);
     return 0;
 }
